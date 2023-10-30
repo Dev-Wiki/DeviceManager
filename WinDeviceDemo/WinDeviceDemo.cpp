0b683b9a8ef043371d5cbb2d9bf1057d821e6ec5
DevWiki <devwiki@qq.com>
2023-10-30 16:44:02 +0800

    添加演示demo项目

---
+++ WinDeviceDemo/WinDeviceDemo.cpp
@@ -0,0 +1,16 @@
+﻿// WinDeviceDemo.cpp: 定义应用程序的入口点。
+//
+
+#include "WinDeviceDemo.h"
+#include "Video/ScreenManager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	ScreenManager screenManager;
+	screenManager.UpdateDisplayInfo();
+	return 0;
+}
